e4914dd6fb9e5d3d4d3d174dc6a3a02486906e60
Rodrigo Espinosa <espinosacurbelo@gmail.com>
2013-12-31 14:48:30 -0200

    Dialog idea

---
+++ client/scripts/models/dialog.js
@@ -0,0 +1,15 @@
+(function ($) {
+	'use strict';
+
+	$.fn.dialog = function (option) {
+		var opts = $.extend({}, $.fn.dialog.defaults, options);
+		
+		$(opts.html).appendTo(opts.appendTo);
+	};
+
+	$.fn.lightbox.defaults = {
+        appendTo: 'body',
+        html: '<input />',
+    };
+
+}(jQuery));
